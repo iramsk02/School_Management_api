a6ebd59fe25b11405bff0c50760f41296dce286e
Iram <iramsabak@gmail.com>
2025-05-23 11:39:18 +0530

    Remove node_modules and .env from repo

--- config/db.js
+++ config/db.js
@@ -1,28 +1,12 @@
-// import mysql from 'mysql2/promise';
-// import dotenv from 'dotenv';
-// dotenv.config();
-// // dotenv.config({ path: path.resolve('./.env') });
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// export default pool;
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-import path from 'path';
-
-dotenv.config({ path: path.resolve(process.cwd(), '.env') });
-
-console.log('Env vars:', process.env.DB_HOST, process.env.DB_USER);
-
+dotenv.config();
+// dotenv.config({ path: path.resolve('./.env') });
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  user: "root",
+  password: "#Ir@msabak02",
+  database: "student_management",
 });
 
 export default pool;
